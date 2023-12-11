3aebc8cc6c5ff76ec32ae93333df455ef3677ea9
stas <stas.bulgakovv@gmail.com>
2023-12-11 12:23:18 +0500

    delete useless imports and functions

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,10 +1,4 @@
-import crypto from "crypto"
-import config from "../data/config.js";
 import Message from "../db/schemas/message.js";
-
-const md5 = data => crypto.createHash('md5')
-    .update(data)
-    .digest("hex")
 
 async function getMessages (req, res) {
     const messages = await Message.find()
